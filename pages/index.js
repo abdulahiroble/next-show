c295c4ac6c3ae0634afc8d983d1b29ec655f1309
abdulahiroble <abdulahi_2610@hotmail.com>
2020-01-20 14:29:56 +0100

    succesfully displayed popular movies using the tmd

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Fetch from "isomorphic-unfetch";
 import React from "react";
 import Layout from "../components/Layout";
 
-const index = () => {
+import Movies from "../components/Movies";
+
+const index = props => {
   return (
     <Layout>
       <h1>Stream Movies on Moviecloud</h1>
@@ -9,11 +12,46 @@ const index = () => {
       <button>Movies</button>
 
       <h2>Upcoming</h2>
+
       <h2>Popular</h2>
+      <div>
+        {props.movie.results.map(test => {
+          return (
+            <ul className="list-group">
+              <li className="list-group-item">
+                <h3> {test.title}</h3>
+                <em>{test.release_date}</em> <br />
+                <a href={test.poster_path}>img</a>
+                <div>{test.poster_path}</div>
+                <p>{test.overview}</p>
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+      {console.log(props.movie)}
+
       <h2>Now Playing</h2>
+
       <h2>Top Rated</h2>
     </Layout>
   );
+};
+
+index.getInitialProps = async function() {
+  // const res = await fetch(
+  //   "https://api.themoviedb.org/3/movie/550?api_key=3e5072126511096a6377f77c742f2864"
+  // );
+
+  const res = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=3e5072126511096a6377f77c742f2864"
+  );
+
+  const data = await res.json();
+
+  return {
+    movie: data
+  };
 };
 
 export default index;
