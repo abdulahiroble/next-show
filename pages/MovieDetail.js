0ea83ef9f16eddc2648b1de33e5889ae683f1695
abdulahiroble <abdulahi_2610@hotmail.com>
2020-02-15 18:47:46 +0100

    fixed url when going to MovieDetail

--- pages/MovieDetail.js
+++ pages/MovieDetail.js
@@ -1,14 +1,16 @@
-import Fetch from "isomorphic-unfetch";
+import fetch from "isomorphic-unfetch";
 import React from "react";
 import Layout from "../components/Layout";
 
 const MovieDetail = ({
   url: {
-    query: { id, title, rating, thumbnail, genre, summary, reviews }
-  }
+    query: { id, title, rating, thumbnail, genre, summary, reviews, hej }
+  },
+  props
 }) => {
   return (
     <Layout>
+      {/* {console.log(props.show)} */}
       <div style={{ display: "flex", justifyContent: "space-around" }}>
         <div>
           <img
@@ -40,20 +42,19 @@ const MovieDetail = ({
       <h2>Trailers</h2>
 
       <h2>Popular Reviews</h2>
-      {id}
+      {reviews}
     </Layout>
   );
 };
 
-// MovieDetail.getInitialProps = async function() {
-//   const test = await fetch(
-//     "https://api.themoviedb.org/3/movie/419704/reviews?api_key=3e5072126511096a6377f77c742f2864&language=en-US&page=1"
-//   );
-
-//   const test2 = await test.json();
+// MovieDetail.getInitialProps = async function(query) {
+//   // const { id } = url.query;
+//   const res = await fetch(query.reviews);
+//   const show = await res.json();
 
 //   return {
-//     test2: test2
+//     query,
+//     show
 //   };
 // };
 
