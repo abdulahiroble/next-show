511c6349fc4445acdc18132a5bdec61d403bfd07
abdulahiroble <abdulahi_2610@hotmail.com>
2020-06-15 12:04:59 +0200

    testing next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,32 @@
 const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const sitemap = require("nextjs-sitemap-generator");
+// const withSass = require("@zeit/next-sass");
+// const sitemap = require("nextjs-sitemap-generator");
 
-module.exports = withImages(
-  sitemap(
-    withSass({
-      env: {
-        API_SECRET: process.env.API_SECRET,
-      },
+module.exports = withImages({
+  env: {
+    API_SECRET: process.env.API_SECRET,
+  },
+});
 
-      baseUrl: "https://seriermanskalse.dk",
-      pagesDirectory: __dirname + "/pages",
-      targetDirectory: "static/",
+// module.exports = withImages(
+//   sitemap(
+//     withSass({
+//       env: {
+//         API_SECRET: process.env.API_SECRET,
+//       },
 
-      exportPathMap: function () {
-        return {
-          "/": { page: "/" },
-        };
-      },
-    })
-  )
-);
+//       baseUrl: "https://seriermanskalse.dk",
+//       pagesDirectory: __dirname + "/pages",
+//       targetDirectory: "static/",
+
+//       exportPathMap: function () {
+//         return {
+//           "/": { page: "/" },
+//         };
+//       },
+//     })
+//   )
+// );
 
 // module.exports = withSass(
 //   withImages({
