10a7bb8875720a272b9a1dc8dcfbafbc9f5c7613
abdulahiroble <abdulahi_2610@hotmail.com>
2020-06-15 11:34:01 +0200

    big updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,41 @@
 const withImages = require("next-images");
+const withSass = require("@zeit/next-sass");
+const sitemap = require("nextjs-sitemap-generator");
 
-module.exports = withImages({
-  env: {
-    API_SECRET: process.env.API_SECRET,
-  },
-});
+module.exports = withImages(
+  sitemap(
+    withSass({
+      baseUrl: "https://seriermanskalse.dk",
+      pagesDirectory: __dirname + "/pages",
+      targetDirectory: "static/",
+
+      exportPathMap: function () {
+        return {
+          "/": { page: "/" },
+        };
+      },
+
+      env: {
+        API_SECRET: process.env.API_SECRET,
+      },
+    })
+  )
+);
+
+// module.exports = withSass(
+//   withImages({
+//     env: {
+//       API_SECRET: process.env.API_SECRET,
+//     },
+//     exportPathMap: function () {
+//       return {
+//         "/": { page: "/" },
+//       };
+//     },
+//     // sitemap ({
+//     //   baseUrl: "https://seriermanskalse.dk/",
+//     //   pagesDirectory: __dirname + "/pages",
+//     //   targetDirectory : 'static/'
+//     // })
+//   })
+// );
