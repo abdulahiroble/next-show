3f5650b76acf6e7068af144bd95333bbf4eb3729
abdulahiroble <abdulahi_2610@hotmail.com>
2020-06-16 16:25:02 +0200

    added url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withSass = require("@zeit/next-sass");
 const sitemap = require("nextjs-sitemap-generator");
 
 sitemap({
-  baseUrl: "<your_website_base_url>",
+  baseUrl: "https://seriermanskalse.dk",
   pagesDirectory: __dirname + "/pages",
   targetDirectory: "static/",
 });
