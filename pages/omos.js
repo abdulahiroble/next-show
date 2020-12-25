13bf97d12a75f404490d30a8427f278cdf1129c4
abdulahi roble <abdulahi_2610@hotmail.com>
2020-12-25 22:32:42 +0100

    Create first story

--- pages/omos.js
+++ pages/omos.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { Button } from "../stories/Button";
 
 const profile = () => {
   return (
---
+++ stories/Button.stories.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Button } from "./Button";
+
+export default {
+    title: "Button",
+};
+
+export const blackButton = () => (
+    <div>
+        <Button variant="black">
+            button
+        </Button>
+    </div>
+);
+
+export const whiteButton = () => (
+    <div>
+        <Button variant="white">
+            button
+        </Button>
+    </div>
+);
+
+
+export const DefaultComponent = () => {
+    return (
+        <div >
+            <Button onClick >Button</Button>
+        </div>
+    )
+};
