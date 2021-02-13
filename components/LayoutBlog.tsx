eaa8901c6bb4ab71b9b45151a942cbc9775ebe0b
abdulahi roble <abdulahi_2610@hotmail.com>
2021-02-13 19:45:25 +0100

    Testing

--- components/LayoutBlog.tsx
+++ components/LayoutBlog.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
-// import LayoutSite from "../components/Layout";
+import LayoutSite from "../components/Layout";
 
 const name = "Serier man skal se";
 export const siteTitle = "Serie man skal se";
@@ -15,32 +15,34 @@ export default function LayoutBlog({
   home?: boolean;
 }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="Serie man skal se"
-          content="Få overblik hvilke serier der er værd at se baseret på bruger score"
-        />
-        <meta name="og:title" content={siteTitle} />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <h3 className={utilStyles.heading2Xl}>{name}</h3>
-          </>
-        ) : (
-          <></>
+    <LayoutSite>
+      <div className={styles.container}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="Serie man skal se"
+            content="Få overblik hvilke serier der er værd at se baseret på bruger score"
+          />
+          <meta name="og:title" content={siteTitle} />
+        </Head>
+        <header className={styles.header}>
+          {home ? (
+            <>
+              <h3 className={utilStyles.heading2Xl}>{name}</h3>
+            </>
+          ) : (
+            <></>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← Tilbage til forside</a>
+            </Link>
+          </div>
         )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Tilbage til forside</a>
-          </Link>
-        </div>
-      )}
-    </div>
+      </div>
+    </LayoutSite>
   );
 }
